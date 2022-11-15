751059e2d8c7d07ade6a8e52addc5fe2740c16b4
Nafis-Fahim50 <nafisf96@gmail.com>
2022-11-15 23:30:02 +0600

    get appointoption data

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 const express = require('express');
 const app = express()
+const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const cors = require('cors')
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.6llxg7j.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6llxg7j.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// console.log(uri);
 
-app.get('/',(req,res)=>{
-    res.send('doctors portal server is running')
+async function run() {
+    try {
+        const appointmentOptionCollection = client.db('doctorPortal').collection('appointmentOptions');
+
+        app.get('/appointoptions', async (req, res) => {
+            const query = {};
+            const cursor = appointmentOptionCollection.find(query);
+            const options = await cursor.toArray();
+            res.send(options);
+            console.log(options);
+        })
+    }
+    finally{
+        
+    }
+}
+run().catch(error => console.error(error))
+app.get('/', (req, res) => {
+    res.send('Doctor portal server is running')
 })
 
-app.listen(port, ()=>{
-    console.log(`server is running on ${port}`)
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`)
 })
+
+
