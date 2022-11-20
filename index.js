8260b0762a8616abee8c139c564fc3e3b05fef72
Nafis-Fahim50 <nafisf96@gmail.com>
2022-11-21 02:15:53 +0600

    implementation payment gateway and update booking collection

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+const { default: Stripe } = require('stripe');
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const port = process.env.PORT || 5000;
 
@@ -35,6 +37,7 @@ async function run() {
         const bookingCollection = client.db('doctorPortal').collection('bookings');
         const usersCollection = client.db('doctorPortal').collection('users');
         const doctorsCollection = client.db('doctorPortal').collection('doctors');
+        const paymentsCollection = client.db('doctorPortal').collection('payments');
         
         const verifyAdmin = async (req,res,next) =>{
             const decodedEmail = req.decoded.email;
@@ -85,6 +88,13 @@ async function run() {
            
             const bookings = await bookingCollection.find(query).toArray();
             res.send(bookings);
+        })
+
+        app.get('/bookings/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const booking = await bookingCollection.findOne(query);
+            res.send(booking);
         })
 
 
@@ -178,6 +188,38 @@ async function run() {
             const filter = { _id: ObjectId(id)}
             const result = await doctorsCollection.deleteOne(filter);
             res.send(result)
+        })
+
+        app.post('/create-payment-intent', async(req,res)=>{
+            const booking = req.body;
+            const price = booking.price;
+            const amount = price * 100;
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency : 'usd',
+                amount : amount,
+                "payment_method_types": [
+                    "card"
+                  ],
+            })
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+              });
+        })
+
+        app.post('/payments', async(req,res)=>{
+            const payment = req.body;
+            const result = await paymentsCollection.insertOne(payment);
+            const id = payment.bookingId;
+            const filter = {_id: ObjectId(id)}
+            const updateDoc = {
+                $set:{
+                    paid:true,
+                    transationId: payment.transationId
+                }
+            }
+            const updateResult = await bookingCollection.updateOne(filter,updateDoc);
+            res.send(result);
         })
         
     }
